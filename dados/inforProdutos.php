6f6c124001037d315cc481908036f37dc918b490
unknown <unicesumar.carlosm@gmail.com>
2021-08-25 18:02:24 -0300

    Pasta de dados enviada com as informações dos produtos

---
+++ dados/inforProdutos.php
@@ -0,0 +1,32 @@
+<?php 
+	$produtos = array(
+			array("titulo" => "BANANADA", "informacao" => "Um clássico é um clássico, né, minha gente?
+			A nossa querida e deliciosa Bananada está disponível na versão tablete de 200g e, além de ser saudável,
+			feita somente com banana e nenhuma adição de açúcar, é enriquecida com proteínas carboidratos e muitas fibras. 
+			Sua saúde e seu paladar vão agradecer.
+			Ingredientes: Banana. 
+			Características: Produto sem conservantes e feito com a própria fruta, a banana. Não usamos polpas artificiais. 
+			NÃO CONTÉM GLÚTEN. " ,"img" => "images/destaque_bananada1.JPG"),
+			array("titulo" => "DOCE DE CIDRA", "informacao" => "Doce de cidra é um doce típico da culinária brasileira
+			feito a base de cidra e açúcar, com cravo e canela ou erva-doce.
+			Na forma de doce em calda, ralado bem fininho.
+			Além de servir para o consumo direto, o doce pode ser usado na composição de outras sobremesas." ,"img" => "images/destaque_cidra1.JPG"),
+			array("titulo" => "DOCE DE FIGO", "informacao" => "Quer coisa mais tradicional que um bom doce de figo acompanhado de queijo? 
+			Ele é delicioso, macio e doce da medida certa.
+			Natural e sem aditivos, tem apenas figos selecionados, açúcar e água em sua composição" ,"img" => "images/destaque_docefigo1.JPG"),
+			array("titulo" => "GOIABADA", "informacao" => "A goiabada é um doce feito de goiaba[1] típico da comida 
+			caipira, mas também é consumido em vários lugares do mundo. A goiabada surgiu no Brasil quando foi usada
+			pelos colonos portugueses como substituto para confecionar a marmelada.
+			Acompanhada de queijo, em especial o queijo minas, forma o Romeu e Julieta, tradicional sobremesa 
+			brasileira." ,"img" => "images/destaque_goiabada1.JPG"),
+			array("titulo" => "DOCE DE LEITE", "informacao" => "O doce de leite é um doce à base de leite e açúcar e 
+			tradicional em vários países da América Latina. Geralmente é feito ao se ferver leite com açúcar ou 
+			cozinhando leite condensado, sendo usado em balas e outras comidas doces, como bolos, biscoitos ou sorvete 
+			e em alguns lugares, é apreciado com torradas ou utilizado como recheio de churros.
+			As variações mais comuns do doce são a pastosa e a sólida (que pode ser cortada em barras ou pedaços), 
+			que se diferenciam por sua consistência. " ,"img" => "images/destaque_docedeleite1.JPG"),
+		);
+
+	
+
+ ?>
